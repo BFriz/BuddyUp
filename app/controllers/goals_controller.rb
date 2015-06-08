cfd2183a72aa7eeecdedb6cae1b5ec7baea87a86
Ben Frisby <bfrisb@gmail.com>
2015-06-08 19:38:59 +0100

    can't figure out why I have a dot instead of a colon in my url

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,4 +8,18 @@ class GoalsController < ApplicationController
     @goal = Goal.new
   end
 
+  def create
+    goal = Goal.new params[:goal].permit(:category, :name, :duration)
+    # binding.pry
+    if goal.save
+      redirect_to goals_path
+    else
+      render 'new'
+    end
+  end
+  def show
+    # binding.pry
+    @goal = Goal.find(params[:id])
+  end
+
 end
